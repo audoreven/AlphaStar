8c14b7af08b3ddd9d35ce21714eb74fd668c4425
Audrey <blueshadow1125@gmail.com>
2019-07-10 19:12:40 -0700

    updating more problems

--- src/recursion/BaleShare.java
+++ src/recursion/BaleShare.java
@@ -5,18 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BaleShare {
-	static int[] idk() {
-		int[] sum=new int[3];
-		return idk();
+	static int[] sum=new int[3];
+	static int ans=100000;
+	static void DFS(int b, int[] bales){
+		if (b<bales.length) {
+			for (int i=0; i<3; i++) {
+				sum[i]+=bales[b];
+				if (sum[i]<ans) {
+					DFS(b+1, bales);
+				}
+				sum[i]-=bales[b];
+			}
+		}
+		if (b==bales.length) {
+			int temp=Math.max(sum[2],Math.max(sum[0], sum[1]));
+			if (ans==0) {
+				ans=temp;
+			} else {
+				ans=Math.min(temp, ans);
+			}
+		}
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine());
-		int[] barn=new int[3];
-		barn[0]=Integer.parseInt(br.readLine());
+		sum[0]=Integer.parseInt(br.readLine());
 		int[] bales=new int[n-1];
 		for (int i=0; i<n-1; i++) {
 			bales[i]=Integer.parseInt(br.readLine());
 		}
+		DFS(0, bales);
+		System.out.println(ans);
 	}
 }
--- src/recursion/DNAAssembly.java
+++ src/recursion/DNAAssembly.java
@@ -3,8 +3,47 @@ package recursion;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class DNAAssembly {
+	static ArrayList<String> seq=new ArrayList<String>();
+	static int length=1000000;
+	static void DFS(int d, String[] dna, boolean[] vis){
+		if (d<dna.length) {
+			for (int i=0; i<dna.length; i++) {
+				// generate permutations
+				if (!vis[i]) {
+					vis[i]=true;
+					seq.add(dna[i]);
+					DFS(d+1,dna, vis);
+					seq.remove(dna[i]);
+					vis[i]=false;
+				}
+			}
+		}
+		if (d==dna.length) {
+			length=Math.min(combine(seq),length);
+			vis=new boolean[dna.length];
+		}
+	}
+	static int combine(ArrayList<String> dna) {
+		String last="";
+		for (int i=0; i<dna.size(); i++) {
+			last=combine(last, dna.get(i));
+		}
+		return last.length();
+	}
+	static String combine(String a, String b) {
+		for (int i=Math.min(a.length(), b.length()); i>0; i--) {
+			if (a.length()==0) {
+				return b;
+			}
+			if (a.substring(a.length()-i,a.length()).equals((b.substring(0, i)))) {
+				return a+b.substring(i, b.length());
+			}
+		}
+		return a+b;
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine());
@@ -12,5 +51,8 @@ public class DNAAssembly {
 		for (int i=0; i<n; i++) {
 			dna[i]=br.readLine();
 		}
+		boolean[] vis=new boolean[dna.length];
+		DFS(0,dna, vis);		
+		System.out.println(length);
 	}
 }
